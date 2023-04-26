cbbe7134ff1679b8b7f0fa836b03f936284df435
Omkar <omkargadekar1701@gmaiil.com>
2023-04-26 14:42:20 +0530

    Initial commit

---
+++ game.js
@@ -0,0 +1,35 @@
+$("h1").css("color", "yellow");
+var userClickedPattern = [];
+var gamePattern = [];
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColor);
+}
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+});
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
